0fbe856ebec46951acdfae78e4c022a75eb80a17
Jorgedst <dtapiaj@uninorte.edu.co>
2026-04-08 00:06:26 -0500

    fix: database conexion.py fixed for .env parameters

--- database/conexion.py
+++ database/conexion.py
@@ -1,13 +1,19 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # carga el .env
+
 def get_connection():
-    try :
+    try:
         connection = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'abcbc6f5',
-            db = 'biblioGestor'
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))
         )
         return connection
     except Exception as ex:
-        print("Error de conexión: ",ex)
+        print("Error de conexión:", ex)
         return None
